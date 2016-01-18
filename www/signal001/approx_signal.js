ea952e2726bd74d9e5b1eb1c75907c3a9723a68b
Evgenii Sopov <mrseakg@gmail.com>
2016-01-19 00:30:40 +0600

    added example web page

---
+++ www/signal001/approx_signal.js
@@ -0,0 +1,24 @@
+function approx_signal(t){
+	return -2289.85*Math.sin(-3.59*t+2320.94)
+	+1563.87*Math.sin(3.47*t-1324.75)
+	-2292.26*Math.cos(3.5*t-5278.38)
+	+1525.23*Math.sin(3.68*t-743.69)
+	+1321.57*Math.cos(-3.8*t-5490.13)
+	+1521.06*Math.sin(3.77*t+6189.22)
+	+911.705*Math.cos(-1.19*t-1998.41)
+	-6149.82*Math.sin(0*t+6782.31)
+	-1364.77*Math.sin(-3.44*t-2279.25)
+	+2687.36*Math.cos(0*t+6327.45)
+	-2080.57*Math.cos(0*t+1647.4)
+	-1595.01*Math.sin(0*t+2630.92)
+	+1727.87*Math.sin(3.72*t+6135.18)
+	+2025.47*Math.sin(0*t-2268.67);
+};
+
+function getDiffValue(){
+	return 2299;
+};
+
+function getDt(){
+	return "2016-01-10 17:21:46";
+}
